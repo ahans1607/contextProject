6a96d93d0b15e15ec8174825d142c8afff03810c
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-03-08 17:58:04 -0600

    04

--- solution/problems/04-bound-by-arg.js
+++ solution/problems/04-bound-by-arg.js
@@ -19,28 +19,9 @@ console.log(spyCar("potato")); // prints "I spy a car"
 
 ***********************************************************************/
 
-function boundByAnArg(func, arg){
-  return func.bind(null, arg)
-
-  
+function boundByAnArg(func, arg) {
+  return func.bind(null, arg);
 }
 
-function iSpy(thing) {
-  return "I spy a " + thing;
-}
-
-let spyTree = boundByAnArg(iSpy, "tree");
-console.log(spyTree()); // prints "I spy a tree"
-console.log(spyTree("car")); // prints "I spy a tree"
-
-let spyCar = boundByAnArg(iSpy, "car");
-console.log(spyCar()); // prints "I spy a car"
-console.log(spyCar("potato")); // prints "I spy a car"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-
-try {
-  module.exports = boundByAnArg;
-} catch (e) {
-  // catch the ref err
-  module.exports = null;
-}
+module.exports = boundByAnArg;
